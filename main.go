c64ece2c6fd74343728c14da07a0867fe72b8b64
xinnj <xinnj@hotmail.com>
2024-03-13 21:27:00 +0800

    Support Ubuntu

--- main.go
+++ main.go
@@ -94,12 +94,13 @@ func installDependencies() {
 		pythonRepoParam = " -i " + appConfig.Python_repo
 	}
 
-	cmds := []string{"yum install -y python3-pip podman podman-docker sshpass rsync",
+	cmds := []string{
+		"if [ -n \"$(which yum 2>/dev/null)\" ]; then pkg_mgr=yum; else pkg_mgr=apt; fi; $pkg_mgr install -y python3-pip podman podman-docker sshpass rsync",
 		"touch /etc/containers/nodocker",
 		"pip3 install -U -r " + filepath.Join(kubesprayPath, pythonRequirements) + pythonRepoParam,
 		"pip3 install -U -r " + filepath.Join(kubesprayPath, "contrib/inventory_builder/requirements.txt") + pythonRepoParam}
 	for _, cmd := range cmds {
-		fmt.Println(cmd)
+		// fmt.Println(cmd)
 		execCommand(cmd, 0)
 	}
 
